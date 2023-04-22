2d9b1c4c3a8da3f355c8298952c56b8636b9cb84
Suprateek <suprateekgoyal@gmail.com>
2023-04-22 13:23:37 +0530

    Hash security added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");   --> 
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +42,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save()
     .then(() =>{
